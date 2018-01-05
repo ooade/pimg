b00cc74c89cc4a44b716e3276662bdaa2552d8e9
Ademola Adegbuyi <ooade96@gmail.com>
2018-01-05 12:07:41 +0100

    Support Vue ⚡️

---
+++ src/vue/rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel'
+import vue from 'rollup-plugin-vue'
+import path from 'path'
+
+let pkg = require('../../package.json')
+
+const rename = name =>
+	path.resolve(name.replace('dist/', 'dist/vue/').replace('/pimg', '/index'))
+
+export default {
+	input: path.join(__dirname + '/index.vue'),
+	external: ['vue', 'whatwg-fetch'],
+	plugins: [
+		vue(),
+		babel({
+			exclude: 'node_modules/**',
+			presets: ['backpack']
+		})
+	],
+	output: [
+		{
+			file: rename(pkg.main),
+			format: 'cjs',
+			sourcemap: true
+		},
+		{
+			file: rename(pkg.module),
+			format: 'es',
+			sourcemap: true
+		},
+		{
+			name: 'pimg',
+			file: rename(pkg.umd),
+			format: 'umd',
+			sourcemap: true,
+			globals: {
+				vue: 'vue'
+			}
+		}
+	]
+}

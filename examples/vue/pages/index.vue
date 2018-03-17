1d683cb1ec20c1fe488f5d6e2122398c712e1fd7
Ademola Adegbuyi <ooade96@gmail.com>
2018-03-17 01:34:51 +0100

    update vue to the new features

--- examples/vue/pages/index.vue
+++ examples/vue/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
 	<div>
-		<h1>Hello, World!</h1>
-		<pimg src="https://res.cloudinary.com/stackpie/image/upload/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
-			placeholder="https://res.cloudinary.com/stackpie/image/upload/c_thumb,w_20/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
-		/>
-	</div>
+		<div class="item">
+			<h1>Simple Implementation</h1>
+			<pimg src="https://res.cloudinary.com/stackpie/image/upload/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
+				placeholder="https://res.cloudinary.com/stackpie/image/upload/c_thumb,w_20/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
+			/>
+		</div>
+		<div class="item">
+			<h1>DataSaver Mode</h1>
+		<pimg dataSaver src="https://res.cloudinary.com/stackpie/image/upload/v1513979515/-895520106_m1whb3.jpg"/>
+		</div>
+		<div class="item">
+			<h1>FetchOnDemand Mode</h1>
+			<pimg fetchOnDemand src="https://res.cloudinary.com/stackpie/image/upload/v1513965940/1_znfymp.jpg"/>
+		</div>
+		</div>
 </template>
 
 <style>
@@ -12,8 +22,31 @@
 	width: 500px;
 }
 
-.pimg__loading {
-	filter: blur(5vw);
+.pimg__placeholder {
+	filter: blur(2vw);
 	transform: scale(1.05);
+}
+
+.pimg__wrapper {
+	display: inline-block !important;
+	position: relative;
+}
+
+.pimg__btn {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	border: 2px solid #fff;
+	border-radius: 20px;
+	font-size: 14px;
+	padding: 0px 8px 2px;
+	cursor: pointer;
+	background: #333;
+	color: #fdfdfd;
+}
+
+.item {
+	margin: 10rem;
 }
 </style>
--- src/vue/index.vue
+++ src/vue/index.vue
@@ -1,47 +1,124 @@
 <template>
-	<span>
+	<div v-if="dataSaverMode" :class="wrapperClassName">
 		<img
 		  v-if="loading"
-		  class="pimg pimg__loading"
+			ref="image"
+		  :class="classNamesCombined"
 		  :src="placeholder||placeholderData"
 		/>
 		<img
 		  v-else
-		  class="pimg"
+		  :class="className"
 		  :src="blob"
 		/>
-	</span>
+		<button :class="buttonClassName" @click="fetchImage(src); dataSaverMode = false">Load image</button>
+	</div>
+	<div v-else>
+		<img
+		  v-if="loading"
+			ref="image"
+		  :class="classNamesCombined"
+		  :src="placeholder||placeholderData"
+		/>
+		<img
+		  v-else
+		  :class="className"
+		  :src="blob"
+		/>
+	</div>
 </template>
 
 <script>
+import config from '../config.js'
+
 export default {
 	name: 'pimg',
 
 	data: () => ({
-		loading: true,
 		blob: null,
-		placeholderData: null
+		buttonClassName: 'pimg__btn',
+		className: 'pimg',
+		classNamesCombined: 'pimg pimg__btn',
+		dataSaverMode: false,
+		delayed: false,
+		loading: true,
+		placeholderClassName: 'pimg__placeholder',
+		placeholderData: null,
+		wrapperClassName: 'pimg__wrapper'
 	}),
 
-	props: ['src', 'placeholder'],
+	props: ['dataSaver', 'fetchOnDemand', 'placeholder', 'src'],
+
+	methods: {
+		delayFetchingImage(delayed) {
+			this.delayed = delayed
+		},
+		fetchImage() {
+			fetch(this.src)
+				.then(res => res.blob())
+				.then(res => {
+					let blob = URL.createObjectURL(res)
+
+					this.blob = blob
+					this.loading = false
+				})
+		},
+		_fetchOnDemand(src) {
+			try {
+				let observer = new IntersectionObserver(entries => {
+					let image = entries[0]
+
+					if (image.isIntersecting) {
+						this.fetchImage(this.src)
+						this.delayFetchingImage(false)
+						observer.disconnect()
+					}
+				})
+
+				observer.observe(this.$refs.image)
+			} catch (_) {
+				// Fail gracefully
+				console.warn('ScrollToView not supported on this browser', _)
+				this.fetchImage(this.src)
+				this.delayFetchingImage(false)
+			}
+		}
+	},
 
 	mounted() {
-		if (!this.placeholder) {
-			this.placeholderData = this.src.replace(
-				'/upload/',
-				'/upload/c_thumb,w_30/'
-			)
+		const {
+			getButtonClassName,
+			getClassName,
+			getDataSaver,
+			getFetchOnDemand,
+			getPlaceholderClassName,
+			getWrapperClassName
+		} = config()
+
+		this.buttonClassName = getButtonClassName()
+		this.className = getClassName()
+		this.dataSaverMode = typeof this.dataSaver !== 'undefined' || getDataSaver()
+		this.placeholderClassName = getPlaceholderClassName()
+		this.wrapperClassName = getWrapperClassName()
+		this.classNamesCombined = this.className + ' ' + this.placeholderClassName
+
+		if (this.src && this.src.includes('cloudinary')) {
+			this.placeholderData =
+				this.placeholder ||
+				this.src.replace('/upload/', '/upload/c_thumb,w_30/')
 		}
 
-		fetch(this.src)
-			.then(res => res.blob())
-			.then(res => {
-				let blob = URL.createObjectURL(res)
-
-				this.blob = blob
-				this.loading = false
-			})
-			.catch(err => console.log(err))
+		if (typeof this.dataSaver !== 'undefined' || getDataSaver()) {
+			this.delayFetchingImage(true)
+		} else if (
+			typeof this.fetchOnDemand !== 'undefined' ||
+			getFetchOnDemand()
+		) {
+			this.delayFetchingImage(true)
+			this._fetchOnDemand(this.src)
+		} else {
+			this.fetchImage(this.src)
+		}
 	}
 }
 </script>
